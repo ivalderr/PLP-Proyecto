f6c60de312cbcaa9fece3135731ba402d40c3b6f
Ismael Rafael Valderrama Saltos <ivalderr@espol.edu.ec>
2021-06-21 22:30:23 -0500

    Update

--- Segunda_parte/gramatica.pl
+++ Segunda_parte/gramatica.pl
@@ -1,10 +1,22 @@
+% operation statement / parentesis multiples / return opcional / value en condicional
 function ::=
-  <datatype> function_name '(' {<arg>} ')' '{'
+  <datatype> function_name '('
+    [<datatype> arg_name>{',' <datatype> arg_name}]
+  ')' '{'
     {<statement>}
-    return ( <variable> | <value> | <function_call_statement> ) ;
+    return
+      <variable> | <value> |
+      <conditional> | <function_call_statement> ';'
   '}'
 
-arg ::= <datatype> arg_name ,
+function_call_statement ::=
+  Fname '(' [<arg>] ')' ';' |
+  <variable> '=' Fname '(' [<arg>] ')' ';'
+
+arg ::=
+  <variable>{',' <arg>} |
+  <value>{',' <arg>} |
+  <function_call_statement>{',' <arg>}
 
 statement ::=
   <assignment_statement> |
@@ -12,24 +24,27 @@ statement ::=
   <conditional_statement> |
   <function_call_statement>
 
-<assignment_statement> ::=
+assignment_statement ::=
+  
+
+conditional :: =
+  [!] (<value> | <variable> | <function_call_statement>)
+      {<relational_operator> <conditional>}
 
 <loop_statement> ::=
-  while ( conditional ) '{' <statement> '}' |
+  while ( <conditional> ) '{' <statement> '}' |
   do '{' <statement> '}' while ( conditional ) ;
 
 <conditional_statement> ::=
-  if ( conditional ) '{' <statement> '}' |
-  if ( conditional ) '{' <statement> '}'
+  if ( <conditional> ) '{' <statement> '}' |
+  if ( <conditional> ) '{' <statement> '}'
   else '{' <statement> '}' |
-  if ( conditional ) '{' <statement> '}'
-  { else if ( conditional ) '{' <statement> '}' }
+  if ( <conditional> ) '{' <statement> '}'
+  { else if ( <conditional> ) '{' <statement> '}' }
   else '{' <statement> '}'
 
 datatype ::= void | char | int | short | long | float | double
-relational_operator  == | > | < | => | =< | !=
-
-conditional :: =
-function_call_statement ::=
+relational_operator ::= == | > | < | => | =< | !=
+value ::= integer | decimal | string | character
+%%
 variable ::=
-value :: =
