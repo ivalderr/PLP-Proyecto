248433f5bd803f473639a034a30a0cd97baf5212
Ismael Rafael Valderrama Saltos <ivalderr@espol.edu.ec>
2021-06-22 09:37:28 -0500

    Update

--- Segunda_parte/cprogram.c
+++ Segunda_parte/cprogram.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
+int myFunction(void) {
+  int x = 1;
+
+  x;
+  "Hello";
+  3;
+  3.5;
+
+  if(1) {
+    if(1) {printf("Aqui");}
+    if(1) {return 0;}
+    if(1) {printf("Aqui nunca");}
+  }
+  printf("%s\n", "jamas te vere");
+  x = (7>2==x && 1 * 5>1);
+  printf("%d\n", x);
+
+  return x;
+}
+
 void main() {
+  int y = -1;
+  int x = myFunction() + 2 + (x>0 || x<2) + y;
+
+  myFunction();
   char msg[] = "Hello World!";
+  printf("%d\n", x);
   printf("%s\n", msg);
+
+  if(2/4) {printf("si");}
+  else {printf("no");}
+
+  x=1;
+  if(7>2==x && 1 * 5>1){printf("si");}
+  else {printf("no");}
+
+  return;
+
+  if(1) {
+    if(1) {printf("Aqui");}
+    if(1) {return;}
+    if(1) {printf("Aqui nunca");}
+  }
+  printf("%s\n", "jamas te vere");
 }
