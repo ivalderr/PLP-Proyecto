e05989e4463f3f3b395c34c1f6356fb55cf94cba
Ismael Rafael Valderrama Saltos <ivalderr@espol.edu.ec>
2021-06-25 00:58:10 -0500

    Update

--- Segunda_parte/gramatica.pl
+++ Segunda_parte/gramatica.pl
@@ -1,4 +1,7 @@
-unaria binaria +,-
-  postfix prefix ++,--
-  * / %
-% parentesis multiples / arreglos
+variable --> "," , (digit ; letter).
+variable --> (letter ; "_"), variable.
+
+letter(C) :- 96<C,C<123;
+              64<C, C<91;
+              47<C, C<58.
+digit(D) --> [D], {47 < D, D < 58}.
